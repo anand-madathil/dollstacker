f663efaaa4da970beb481094b3e63783e487e3c3
anand.k <anandkm.work@gmail.com>
2016-08-01 11:55:14 +0530

    added a list of issues

--- nesteddolls2.py
+++ nesteddolls2.py
@@ -2,25 +2,28 @@
 #For each doll, if its height and width is less than the height capacity and width capacity of another, change the capacities of the latter doll to the measurements of the former, and delete the former doll entirely.
 #Base case is when the capacities of each doll are less than the measurements of every other doll. In this case, return the length of one of the lists
 #find the minimum of these lengths
+#problems: during the first iteration the program seems to magically delete a value, for no apparent reason. the program also seems to change some values again for no apparent reason.
 import numpy as np
 globalcomparer = []
 def Nestedcheck(height, width, heightcap, widthcap):
 	global globalcomparer
-	for i in range (len(height)):
-		for a in range(i) + range(i + 1, len(height)):
-			#if len(height) == 3:
-			#print "i =",i,"a =",a,height,width,np.delete(height, a),np.delete(width, a)
-			if heightcap[i] > height[a] and widthcap[i] > width[a]: #to find if recursion can continue. I don't believe you can store dolls with height x inside another doll with heightcap x
-				heightstore = height
-				widthstore = width
-				heightcstore = heightcap
-				widthcstore = widthcap
-				heightcstore[i] = height[a]
-				widthcstore[i] = width[a]
-				Nestedcheck(np.delete(heightstore, a), np.delete(widthstore, a), np.delete(heightcstore, a), np.delete(widthcstore, a))
+	print height,width, heightcap, widthcap
+	if len(height) != 1:
+		for i in range (len(height)):
+			for a in range(i) + range(i + 1, len(height)):
+				if heightcap[i] > height[a] and widthcap[i] > width[a]: #to find if recursion can continue. I don't believe you can store dolls with height x inside another doll with heightcap x
+					heightstore = height
+					widthstore = width
+					heightcstore = heightcap
+					widthcstore = widthcap
+					heightcstore[i] = height[a]
+					widthcstore[i] = width[a]
+					Nestedcheck(np.delete(heightstore, a), np.delete(widthstore, a), np.delete(heightcstore, a), np.delete(widthcstore, a))
 				#here you would try to place it inside, and continue the check, so call the function again with the changed vars
-			else:
-				globalcomparer.append(len(height))
+				else:
+					globalcomparer.append(len(height))
+	else:
+		globalcomparer.append(len(height))
 
 def Nestedcompare(height, width, heightcap, widthcap):
 	global globalcomparer
@@ -28,7 +31,7 @@ def Nestedcompare(height, width, heightcap, widthcap):
 	return min(globalcomparer)
 
 if __name__ == "__main__":
-	w = [20,40,30]
 	h = [30,50,40]
+	w = [20,40,30]
 	print Nestedcompare(h,w,h,w)
 			
